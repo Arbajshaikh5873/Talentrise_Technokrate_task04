490ae96ce7f2d3464c03f91eba2a230694b5463a
Arbaj Shaikh <sarbaj637@gmail.com>
2025-12-25 15:02:55 +0530

    added form componet

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-
-import './App.css'
+import "./App.css";
+import AddEdit from "./components/AddEdit";
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
     <>
-    
+      <Navbar />
+      <AddEdit />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
